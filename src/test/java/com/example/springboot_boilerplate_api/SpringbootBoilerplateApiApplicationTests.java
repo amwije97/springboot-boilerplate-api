eef468b8b341e9798cfb57f9e5f4b51b287bdeed
malitha97 <malitha97.work@gmail.com>
2025-06-27 09:35:50 +0530

    feat: initial boiler plate code

---
+++ src/test/java/com/example/springboot_boilerplate_api/SpringbootBoilerplateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_boilerplate_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBoilerplateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
